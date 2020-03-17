65dfc2d597db2aea869b256224d03f432a39e18a
programworld999 <42247997+programworld999@users.noreply.github.com>
2020-03-18 02:57:17 +0530

    Add files via upload

---
+++ mobileview.py
@@ -0,0 +1,19 @@
+import os
+
+def main(mloc):
+	input("Build(just enter): ")
+	try:
+		os.system("npm run build")
+		pass
+	except:
+		print("Unabl To Run 'npm build'")
+		return 0
+	os.system('cp ./build/* "'+mloc+'" -r')
+	print("Deployed");
+	main(mloc)
+
+
+if __name__ == "__main__":
+	mloc = str(input("location: "));
+	print(mloc)
+	main(mloc)
